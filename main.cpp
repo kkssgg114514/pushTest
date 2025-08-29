4bd448901b0a56107ae34dfd39772c70cb405161
kkssgg <2524183995@qq.com>
2025-08-30 00:35:50 +0800

    添加相反数处理

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <vector>
 
 int add(int a, int b) 
 {
     return a + b;
+}
+
+template<typename T>
+void inverse(std::vector<T>& v)
+{
+    for (int i = 0; i < v.size(); i++) {
+        v[i] = -v[i];
+    }
 }
 
 int main() 
